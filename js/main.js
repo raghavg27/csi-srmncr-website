1861a39ce34f53d0b80c614a725d91c59c46fca3
Sarthak Bhatnagar <sarthaksbi@gmail.com>
2020-10-05 12:08:45 +0530

    resolved dot issue and fixed minor bugs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 jQuery(document).ready(function( $ ) {
   
 });
+$(document).on('click', '.dropdown-menu', function (e) {
+    // console.log('dd');
+    e.stopPropagation();
+  });
+  
+  // make it as accordion for smaller screens
+  if ($(window).width() < 992) {
+    $('.dropdown-menu a').click(function(e){
+      e.preventDefault();
+        if($(this).next('.submenu').length){
+          $(this).next('.submenu').toggle();
+        }
+        $('.dropdown').on('hide.bs.dropdown', function () {
+            $(this).find('.submenu').hide();
+        });
+    });
+  }
